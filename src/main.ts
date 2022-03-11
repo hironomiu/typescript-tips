22f775305b06d72b81cd9373924726a77fcfbbab
hironomiu <hironomiu@gmail.com>
2022-03-11 19:17:00 +0900

    commited

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { moduleA } from './modules/moduleA'
 import { moduleB } from './modules/moduleB'
 import { moduleC } from './modules/moduleC'
+import { moduleD } from './modules/moduleD'
 
 moduleA()
 moduleB()
 moduleC()
+moduleD()
---
+++ src/modules/moduleD.ts
@@ -0,0 +1,36 @@
+const map = <T>(array: T[], fn: (item: T) => T): T[] => {
+  let result = []
+  for (let i = 0; i < array.length; i++) {
+    result.push(fn(array[i]))
+  }
+  return result
+}
+
+const map2 = <T, U>(array: T[], fn: (item: T) => U): U[] => {
+  let result = []
+  for (let i = 0; i < array.length; i++) {
+    result.push(fn(array[i]))
+  }
+  return result
+}
+
+type MAP3 = <T, U>(array: T[], fn: (item: T) => U) => U[]
+
+const map3: MAP3 = (array, fn) => {
+  let result = []
+  for (let i = 0; i < array.length; i++) {
+    result.push(fn(array[i]))
+  }
+  return result
+}
+
+export const moduleD = () => {
+  console.log(`---------- moduleDstart ----------`)
+
+  console.log(map([1, 2, 3, 4], (item) => item * 2))
+  // @ts-expect-error
+  console.log(map([1, 2, 3, 4], (item) => item.toString()))
+  console.log(map2([1, 2, 3, 4], (item) => item.toString()))
+  console.log(map3([1, 2, 3, 4], (item) => ++item))
+  console.log(`---------- moduleD end   ----------`)
+}
