843fa856d81ca19e7b7ff09617b27ade004d8cc9
hironomiu <hironomiu@gmail.com>
2022-04-28 17:28:28 +0900

    commited

--- src/ts/modules/rest.ts
+++ src/ts/modules/rest.ts
@@ -1,16 +1,19 @@
 export const rest = () => {
-  let funcA = (arg1: string, arg2: string, ...arg3: any) => {
+  let funcA = <T extends string>(arg1: T, arg2: T, ...arg3: T[]) => {
     return (
       arg1 +
       arg2 +
-      arg3.reduce((acc: any, cur: any) => (acc += `<rest>${cur}</rest>`), '')
+      arg3.reduce((acc, cur) => (acc += `<rest>${cur}</rest>`), '')
     )
   }
   console.log(funcA('a', 'b', 'c', 'd'))
-  let funcB = (arg1: string, ...arg2: any) => {
+  let funcB = (arg1: string, ...arg2: string[]) => {
     return (
       arg1 +
-      arg2.reduce((acc: any, cur: any) => (acc += `<rest>${cur}</rest>`), '')
+      arg2.reduce(
+        (acc: string, cur: string) => (acc += `<rest>${cur}</rest>`),
+        ''
+      )
     )
   }
   console.log(funcB('a', 'b', 'c', 'd'))
